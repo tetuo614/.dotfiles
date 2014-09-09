653d74cc50f30d31493f632424090789febf290c
Tatsuaki Yamanaka <t_yamanaka@fancs.com>
2014-09-09 11:22:35 +0900

    add zsh(.zprezto)

--- setup.sh
+++ setup.sh
@@ -2,7 +2,15 @@
 
 DOT_FILES=(.vimrc .vim .gitconfig)
 
-for file in ${DOT_FILES[@]}
-do
-    ln -s $HOME/.dotfiles/$file $HOME/$file
-done
+if [ $# -eq 0 ]; then
+    echo 'all file'
+    for file in ${DOT_FILES[@]}
+    do
+        ln -s $HOME/.dotfiles/$file $HOME/$file
+    done
+elif [ $# -eq 1 ]; then
+    echo $1
+    ln -s $HOME/.dotfiles/$1 $HOME/$1
+else
+    echo 'Oops! too many argments'
+fi
